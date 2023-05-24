2d569778d972084cbb9ac05dd0146383940a30ed
armando <acalderon.root@gmail.com>
2023-05-23 20:31:30 -0600

    Install phpunit

---
+++ tests/AtmTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AtmTest extends TestCase
+{
+    /** @test */
+    public function can_register_an_user()
+    {
+        $user = new User();
+    }
+}
